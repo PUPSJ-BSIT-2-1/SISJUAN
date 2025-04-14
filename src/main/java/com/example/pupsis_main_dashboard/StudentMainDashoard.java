0129490ea0adf8e855e257109a88c9bcc8b2e161
ChaosCedd <166236276+icodecedd@users.noreply.github.com>
2025-04-14 12:11:17 +0800

    Change the title into PUPSIS MAIN DASHBOARD.

--- src/main/java/com/example/pupsis_main_dashboard/StudentMainDashoard.java
+++ src/main/java/com/example/pupsis_main_dashboard/StudentMainDashoard.java
@@ -12,7 +12,7 @@ public class StudentMainDashoard extends Application {
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(StudentMainDashoard.class.getResource("hello-view.fxml"));
         Scene scene = new Scene(fxmlLoader.load());
-        stage.setTitle("PUPSIS Main Dashboard");
+        stage.setTitle("PUPSIS MAIN DASHBOARD");
         stage.setScene(scene);
         stage.show();
     }
