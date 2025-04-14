48f8ebd1c9a955efa8dd927402408d2da7484f29
harowwld <harolddelapena.11@gmail.com>
2025-04-14 08:59:56 +0800

    V1.0.0

--- src/main/java/com/example/pupsis_main_dashboard/HelloApplication.java
+++ src/main/java/com/example/pupsis_main_dashboard/HelloApplication.java
@@ -4,7 +4,6 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
-import javafx.stage.StageStyle;
 
 import java.io.IOException;
 
@@ -15,8 +14,6 @@ public class HelloApplication extends Application {
         Scene scene = new Scene(fxmlLoader.load());
         stage.setTitle("Hello!");
         stage.setScene(scene);
-//        stage.initStyle(StageStyle.TRANSPARENT);
-//        scene.setFill(javafx.scene.paint.Color.TRANSPARENT);
         stage.show();
     }
 
