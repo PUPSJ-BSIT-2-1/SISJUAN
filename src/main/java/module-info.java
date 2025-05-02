a4348d88a564fe356206df0c5c63007dc18a99ef
harowwld <harolddelapena.11@gmail.com>
2025-05-02 14:23:15 +0800

    Updated database

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -3,7 +3,6 @@ module com.example.pupsis_main_dashboard {
     requires javafx.fxml;
     requires java.sql;
     requires javafx.media;
-    //noinspection Java9RedundantRequiresStatement
     requires java.desktop;
     requires java.prefs;
     requires java.mail;
