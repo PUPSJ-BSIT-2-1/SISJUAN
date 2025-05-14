d4825d6a963927ab02a7fad78c34d7c3d6e4d10d
Rjjackflorida <rjjackflorida7@gmail.com>
2025-05-14 21:12:43 +0800

    Adding a Role-Picker Window

--- src/main/java/com/example/pupsis_main_dashboard/controllers/FrontPageController.java
+++ src/main/java/com/example/pupsis_main_dashboard/controllers/FrontPageController.java
@@ -1,15 +1,28 @@
 package com.example.pupsis_main_dashboard.controllers;
 
 import javafx.animation.FadeTransition;
+import javafx.animation.Interpolator;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.effect.GaussianBlur;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
+import javafx.scene.layout.Pane;
+import javafx.scene.paint.Color;
+import javafx.stage.Modality;
 import javafx.stage.Stage;
+import javafx.stage.StageStyle;
 import javafx.util.Duration;
 import com.example.pupsis_main_dashboard.utility.StageAndSceneUtils;
+import static com.example.pupsis_main_dashboard.utility.StageAndSceneUtils.showAlert;
 
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import java.io.IOException;
 import java.util.Objects;
 
@@ -21,7 +34,8 @@ public class FrontPageController {
     @FXML private Button aboutButton;
     @FXML private Button othersButton;
     @FXML private Button getStartedButton;
-
+    
+    private static final Logger logger = LoggerFactory.getLogger(FrontPageController.class);
     private final StageAndSceneUtils stageUtils = new StageAndSceneUtils();
 
     @FXML
@@ -82,8 +96,38 @@ public class FrontPageController {
     // Load the next stage
     @FXML private void handleGetStartedButton() throws IOException {
         if (getStartedButton.getScene() != null && getStartedButton.getScene().getWindow() != null) {
+        	
+        	//Getting current Stage
             Stage currentStage = (Stage) getStartedButton.getScene().getWindow();
-            stageUtils.loadStage(currentStage, "fxml/StudentLogin.fxml", StageAndSceneUtils.WindowSize.MEDIUM);
+            Stage rolePickStage = new Stage();
+            rolePickStage.initOwner(currentStage);			
+            //Blurring the current stage
+        	Pane currentPane = (Pane) currentStage.getScene().getRoot();
+        	currentPane.setEffect(new GaussianBlur(10));
+            
+            try {
+                FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/com/example/pupsis_main_dashboard/fxml/RolePick.fxml"));
+                Parent root = fxmlLoader.load();
+                RolePickController controller = fxmlLoader.getController();
+                
+                Scene scene =  new Scene(root);
+                scene.setFill(Color.TRANSPARENT);
+                rolePickStage.initStyle(StageStyle.TRANSPARENT);
+                rolePickStage.initModality(Modality.APPLICATION_MODAL);
+            	rolePickStage.setTitle("Role Path");
+            	rolePickStage.getIcons().add(new Image(getClass().getResource("/com/example/pupsis_main_dashboard/Images/PUPSJ Logo.png").toExternalForm()));
+            	
+            	controller.setCloseHandler(rolePickStage);
+            	controller.setPreviousStage(currentStage);
+            	applyTransition(root);
+            	
+                rolePickStage.setScene(scene);
+            	rolePickStage.show();
+            } catch (IOException e) {
+                logger.error("Error loading Role Pick window");
+                showAlert("Error", "Failed to load Role Pick window");
+            }
+            
         }
     }
 
@@ -95,4 +139,14 @@ public class FrontPageController {
             System.err.println("Failed to open URL: " + e.getMessage());
         }
     }
+    
+   //Transition 
+   private static void applyTransition(Parent root) {
+            FadeTransition fadeTransition = new FadeTransition(Duration.millis(700), root);
+            fadeTransition.setFromValue(0.0);
+            fadeTransition.setToValue(1.0);
+            fadeTransition.setInterpolator(Interpolator.EASE_BOTH);
+            fadeTransition.play();
+		
+	}
 }
