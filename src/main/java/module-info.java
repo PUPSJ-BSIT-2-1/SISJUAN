efb0690edd634dfd5e02d50993d25f3700bbbce6
harowwld <harolddelapena.11@gmail.com>
2025-05-15 11:41:51 +0800

    Update FXML controller reference in newEditingGradePage and adjust module exports

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -15,6 +15,5 @@ module com.example.pupsis_main_dashboard {
     exports com.example.pupsis_main_dashboard.controllers;
     opens com.example.pupsis_main_dashboard.controllers to javafx.fxml;
     exports com.example.pupsis_main_dashboard.utilities to com.fasterxml.jackson.databind;
-    exports com.example.pupsis_main_dashboard.controllers.contents;
     opens com.example.pupsis_main_dashboard.controllers.contents to javafx.fxml;
 }
