313924900e7dc2c76804545fbae7704b25921425
harowwld <harolddelapena.11@gmail.com>
2025-05-15 13:36:20 +0800

    Enhance Enrollment UI with new styles and layout adjustments

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,19 +1,23 @@
 module com.example.pupsis_main_dashboard {
-    requires transitive javafx.graphics;
-    requires transitive javafx.controls;
+    requires javafx.controls;
     requires javafx.fxml;
-    requires java.sql;
-    requires javafx.media;
-    requires java.desktop;
-    requires java.prefs;
-    requires java.mail;
-    requires org.slf4j;
     requires com.fasterxml.jackson.databind;
+    requires java.sql;
+
+    // RequiresTransitive helps ensure that modules depending on this one also have access to these modules
+    requires transitive javafx.graphics;
+    requires javafx.base;
+    requires org.slf4j;
+    requires ch.qos.logback.classic;
+    requires jakarta.mail;
+    requires java.desktop;
+    requires javafx.media;
+    requires java.mail;
+    requires java.prefs;
 
     opens com.example.pupsis_main_dashboard to javafx.fxml;
     exports com.example.pupsis_main_dashboard;
     exports com.example.pupsis_main_dashboard.controllers;
     opens com.example.pupsis_main_dashboard.controllers to javafx.fxml;
     exports com.example.pupsis_main_dashboard.utilities to com.fasterxml.jackson.databind;
-    opens com.example.pupsis_main_dashboard.controllers.contents to javafx.fxml;
 }
